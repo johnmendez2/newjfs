74a811ec717aeccdff10fbfafccfe256a11c21f4
johnmendez2 <66302608+johnmendez2@users.noreply.github.com>
2023-06-07 10:17:32 +0400

    sort options

--- src/Pages/Collectionpage.js
+++ src/Pages/Collectionpage.js
@@ -10,17 +10,17 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function CollectionPage() {
   const [products, setProducts] = useState([]);
+  const [sortBy, setSortBy] = useState(""); // Step 1
   const navigate = useNavigate();
   const location = useLocation();
   const [isFetching, setIsFetching] = useState(true);
   let league = "";
+  
   useEffect(() => {
     const getProducts = async () => {
       let query = fs.collection("NEWwebsiteProducts");
-    
-      // Check if league parameter is provided in the URL
-
       
+      // Check if league parameter is provided in the URL
       if (location.pathname.split("/")[2] && location.pathname.split("/")[2] === 'grl'){
         query = query.where("grail", "==",  "yes")
         console.log("working")
@@ -30,7 +30,7 @@ export default function CollectionPage() {
         query = query.where("league", "==", league);
       }
       console.log(query)
-    
+      
       // Check if search parameter is provided in the URL
       const search = window.location.href.split("search=")[1];
       console.log(search)
@@ -40,7 +40,7 @@ export default function CollectionPage() {
           id: doc.id,
           ...doc.data()
         }));
-      
+        
         const fuse = new Fuse(products, {
           keys: ['title','league'],
           includeScore: true,
@@ -48,7 +48,7 @@ export default function CollectionPage() {
           ignoreLocation: true,
           ignoreFieldNorm: true
         });
-      
+        
         const matchingProducts = fuse.search(search).map((result) => result.item);
         setProducts(matchingProducts);
       }
@@ -67,44 +67,62 @@ export default function CollectionPage() {
     
     getProducts();
   }, [location]);
-
+  
+  let sortedProducts = [...products]; // Step 2
+  
+  if (sortBy === "price-low-to-high") {
+    sortedProducts.sort((a, b) => a.price - b.price); // Sort by price ascending
+  } else if (sortBy === "price-high-to-low") {
+    sortedProducts.sort((a, b) => b.price - a.price); // Sort by price descending
+  }
+  
   return (
     <HelmetProvider>
-          <div>
-      <Navbar />
-      <Helmet>
-      <title>Products | John's Football Shirts</title>
-              <meta name="description" content="Browse through beautiful football shirts past and present." />
-            </Helmet>
-      <div className="collection-page">
-        {isFetching ? (
-          <h2>Fetching products...</h2>
-        ) : (
-          <>
-            <h2 className="showing">Showing {products.length} {products.length > 1 ? "products" : "product"}</h2>
-                <div className="product-cards-container">
-                {products.length > 0 ? (
-              products.map((product) => (
-                  <ProductCard
-                    key={product.id}
-                    imageSrc={product.url}
-                    productName={product.title}
-                    price={product.price}
-                    size={product.size}
-                    url = {`/${product.id}-${product.title.toLowerCase().replace(/[^a-z0-9]+/g, '-')}`}
-                  />
-              ))
-            ) : (
-              <h2 >Your search returned no results.</h2>
-            )}
-            </div>
-          </>
-        )}
+      <div>
+        <Navbar />
+        <Helmet>
+          <title>Products | John's Football Shirts</title>
+          <meta name="description" content="Browse through beautiful football shirts past and present." />
+        </Helmet>
+        <div className="collection-page">
+          {isFetching ? (
+            <h2>Fetching products...</h2>
+          ) : (
+            <>
+              <h2 className="showing">Showing {products.length} {products.length > 1 ? "products" : "product"}</h2>
+              <div className="product-cards-container">
+              <div className="sort-container ">
+                <label htmlFor="sort-by-price">Sort by price:</label>
+                <select
+                  id="sort-by-price"
+                  value={sortBy}
+                  onChange={(e) => setSortBy(e.target.value)}
+                >
+                  <option value="">None</option>
+                  <option value="price-low-to-high">Price (Low to High)</option>
+                  <option value="price-high-to-low">Price (High to Low)</option>
+                </select>
+              </div>
+                {sortedProducts.length > 0 ? (
+                  sortedProducts.map((product) => (
+                    <ProductCard
+                      key={product.id}
+                      imageSrc={product.url}
+                      productName={product.title}
+                      price={product.price}
+                      size={product.size}
+                      url={`/${product.id}-${product.title.toLowerCase().replace(/[^a-z0-9]+/g, '-')}`}
+                    />
+                  ))
+                ) : (
+                  <h2>Your search returned no results.</h2>
+                )}
+              </div>
+            </>
+          )}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
     </HelmetProvider>
   );
-  
-  
 }
