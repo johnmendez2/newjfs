2373b7a089a7f16923382a0eaf9633fadde53312
John Mendez <johnbvon@gmail.com>
2023-07-06 18:59:24 +0400

    changed to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
 import ShirtPage from './Pages/ShirtPage';
 import Collectionpage from './Pages/Collectionpage';
