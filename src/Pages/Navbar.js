1eab2f346db8f0a7fd7583b7852be89a3f75cd08
John Mendez <johnbvon@gmail.com>
2023-07-07 08:06:40 +0400

    added popular searches back

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../Assets/126412-fans.json';
 import '../Css/navbar.css';
 import jfslogo from '../Assets/jfslogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faBars, faArrowTrendUp } from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const [searchOpen, setSearchOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [searchClicked, setSearchClicked] = useState(false);
+  const popularSearches = ['fc barcelona', 'international', 'liverpool'];
   const navigate = useNavigate();
   const defaultOptions = {
     loop: true,
@@ -28,12 +30,40 @@ function Navbar() {
     setMenuOpen(!menuOpen);
   };
 
+  const handleSearchbarClick = () => {
+    setSearchClicked(!searchClicked);
+  };
+
   const handleGo = (e) => {
     e.preventDefault();
     const searchText = e.target.previousSibling.value;
     navigate(`/products/search=${searchText}`);
     setSearchOpen(false);
   };
+
+  const handlePopularSearch = (searchText) => {
+    navigate(`/products/search=${searchText}`);
+    setSearchOpen(false);
+  };
+
+  const searchContainerRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchContainerRef.current &&
+        !searchContainerRef.current.contains(event.target)
+      ) {
+        setSearchClicked(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div>
@@ -65,10 +95,32 @@ function Navbar() {
 <a href="/blogs" className="faqs">BLOGS</a>
         </div>
       <div className={`search-container ${searchOpen ? 'open' : ''}`}>
-        <form>
+        <form onClick={handleSearchbarClick}>
           <input type="text" placeholder="Search store here..." />
           <button type="submit" onClick={handleGo}>Go</button>
         </form>
+        {searchClicked && (
+          <div className="popular-searches">
+            <div className="popular-searches-header">
+              Popular Searches <FontAwesomeIcon icon={faArrowTrendUp} width={'25px'} />
+            </div>
+            <ul>
+              {popularSearches.map((search) => (
+                <li
+                  key={search}
+                  onClick={() => {
+                    handlePopularSearch(search);
+                    handleSearchbarClick();
+                  }}
+                >
+                  <FontAwesomeIcon icon={faSearch} width={'16px'} />
+                  {' '}
+                  {search}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
